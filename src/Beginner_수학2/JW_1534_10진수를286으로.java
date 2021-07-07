a645c99ff6f75362e089a8dbc4bdc60fd5428d96
KWONTAEDOO <td0554@dongguk.edu>
2021-07-07 23:30:53 +0900

    알고리즘

--- src/Beginner_수학2/JW_1534_10진수를286으로.java
+++ src/Beginner_수학2/JW_1534_10진수를286으로.java
@@ -2,9 +2,9 @@ package Beginner_수학2;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class JW_1534_10진수를286으로 {
@@ -12,17 +12,86 @@ public class JW_1534_10진수를286으로 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		System.setIn(new FileInputStream("수학2/10진수를.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
-		int N = Integer.parseInt(br.readLine());
-		int[] count = new int[N];
-		// 3점 2점  1점 중 최종점수가 가장많은사람이 회장ㅇ된다. 
-		// 배열에 저장해놓고 만약 동점이라면 횟수를 세어주면될듯.
-		// 유일한 경우에는 후보의 번호와 최고 점수출력. (3 -> 2-> 1) 순으로 비교
-		// 만약세개가 다같다면 0과 최고점수출력
+		st = new StringTokenizer(br.readLine());
 		
+		int N = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
 		
+		if(B==2) {
+			//2진수로 바꾸기.
+			String str1 = "";
+			while(true) {
+				if(N==1) {
+					str1 += 1;
+					break;
+				}
+				int a = N % 2;
+				N /= 2;
+				str1 += a;
+			}
+			StringBuilder sb = new StringBuilder();
+			for (int i = str1.length()-1; i >= 0; i--) {
+				sb.append(str1.charAt(i));
+			}
+			System.out.println(sb);
+		}
+		else if(B==8) {
+			String str1 = "";
+			while(true) {
+				if(N==0) {
+					break;
+				}
+				int a = N % 8;
+				N /= 8;
+				str1 += a;
+			}
+			StringBuilder sb = new StringBuilder();
+			for (int i = str1.length()-1; i >= 0; i--) {
+				sb.append(str1.charAt(i));
+			}
+			System.out.println(sb);
+		}
+		else {
+			String str1 = "";
+			while(true) {
+				if(N==0) {
+					break;
+				}
+				int a = N % 16;
+				N /= 16;
+				if(a>9) {
+					//A=10; B=11,C=12,D=13,E=14,F=15,G=16
+					switch(a) {
+					case 10 :
+						str1 += 'A';
+						continue;
+					case 11 :
+						str1 += 'B';
+						continue;
+					case 12 :
+						str1 += 'C';
+						continue;
+					case 13 :
+						str1 += 'D';
+						continue;
+					case 14 :
+						str1 += 'E';
+						continue;
+					case 15 :
+						str1 += 'F';
+						continue;
+					}
+				}
+				str1 += a;
+			}
+			StringBuilder sb = new StringBuilder();
+			for (int i = str1.length()-1; i >= 0; i--) {
+				sb.append(str1.charAt(i));
+			}
+			System.out.println(sb);
+		}
 	}
 }
 
--- src/IM_분할정복/JW_3518_퀵정렬.java
+++ src/IM_분할정복/JW_3518_퀵정렬.java
@@ -2,18 +2,67 @@ package IM_분할정복;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class JW_3518_퀵정렬 {
-
-	public static void main(String[] args) throws FileNotFoundException {
+	
+	static int[] arr;
+	static int N;
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		System.setIn(new FileInputStream("분할정복/퀵정렬.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
+		N = Integer.parseInt(br.readLine());
 		
+		arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		//첫 번째로 피봇의 위치를 파악하기 위해 분할탐색해줘야한다.
+		//맨마지막을 타겟으로 두고 앞에서부터 비교하면서 작으면 앞으로 크면 뒤로 빼주기.
+		//작은 숫자의 index를 저장해서 갖고다니기. 큰숫자의 index는 바꾼인덱스부터 +1 씩 넣어주기.
+		
+		//분할과정
+		quickSort(0, N-1);
+	}
+	
+	private static void quickSort(int low, int high) {
+		if(low>=high) return;
+		int pivot = arr[high];
+		int i = low-1;
+		
+		// 5 2 4 1    pivot = 3 
+		for (int j = low; j < high; j++) {
+			if(arr[j]<pivot) {
+				++i;
+				Swap(i,j);
+			}
+		}
+		++i;
+		Swap(i,high);
+		StringBuilder sb = new StringBuilder();
+		for (int j = 0; j < arr.length; j++) {
+			sb.append(arr[j]+" ");
+		}
+		System.out.println(sb);
+		quickSort(low, i-1);
+		quickSort(i+1, high);
 	}
 
+	private static void Swap(int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
 }
+
+
+
+
+
+
