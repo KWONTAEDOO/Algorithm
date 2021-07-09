e33da17bed37bad8804b0ec9660ed7e5377e2b2a
td0554 <td0554@naver.com>
2021-07-10 00:05:55 +0900

    commit

--- src/IM_분할정복/JW_1219_모자이크.java
+++ src/IM_분할정복/JW_1219_모자이크.java
@@ -1,13 +1,36 @@
 package IM_분할정복;
 
+import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class JW_1219_모자이크 {
+	
+	static class Point implements Comparable<Point>{
+		int x,y;
+		
+		Point(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public String toString() {
+			return "x : "+x+"y: "+y;
+		}
+
+		@Override
+		public int compareTo(Point o) {
+			return this.y - o.y;
+		}
+	}
 	
 	public static void main(String[] args) throws IOException {
 		System.setIn(new FileInputStream("분할정복/모자이크.txt"));
@@ -15,15 +38,41 @@ public class JW_1219_모자이크 {
 		StringTokenizer st;
 		
 		//일단 열이 최대가 되는 크기로 확인해본다.
-		//리스트에 넣은거 방문처리를 해나간다. 방문했다면 PASS , 겹치는게 가능.
-		
 		st = new StringTokenizer(br.readLine());
 		
-		int r = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken()); //최대 행
+		int c = Integer.parseInt(st.nextToken()); //최대 열
 		
+		int n = Integer.parseInt(br.readLine()); //색종이 장수.
 		
+		int wrong = Integer.parseInt(br.readLine()); //잘못칠해진칸의 개수
 		
+		List<Point> list = new ArrayList<>();
+		
+		int max_r = 0; //가장 높은 행값 배열의 시작을 위해?(맨 밑에서 시작)
+		for (int i = 0; i < wrong; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			list.add(new Point(x,y));
+			max_r = Math.max(max_r, x);
+		}
+		
+		Collections.sort(list);
+		
+		int current_c = list.get(0).y+2;
+		for (int i = 0; i < list.size(); i++) {
+			if(current_c < list.get(i).y) {
+				current_c = list.get(i).y;
+			}
+			
+		}
 		
 	}
 }
+
+
+
+
