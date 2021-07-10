9297b63e8bc76831fa39a9ec5ae5a3f387c3076b
td0554 <td0554@naver.com>
2021-07-10 11:33:41 +0900

    모자이쿠

--- src/IM_분할정복/JW_1219_모자이크.java
+++ src/IM_분할정복/JW_1219_모자이크.java
@@ -1,14 +1,10 @@
 package IM_분할정복;
 
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class JW_1219_모자이크 {
@@ -36,8 +32,12 @@ public class JW_1219_모자이크 {
 		System.setIn(new FileInputStream("분할정복/모자이크.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
+		StringBuilder sb = new StringBuilder();
 		//일단 열이 최대가 되는 크기로 확인해본다.
+		
+		//리스트에 시작점기준으로 사각형둘러싸고 하나씩 키워나가기.
+		//색종이를 다안써도되는과정을 생각못했음.
+		
 		st = new StringTokenizer(br.readLine());
 		
 		int r = Integer.parseInt(st.nextToken()); //최대 행
@@ -47,29 +47,57 @@ public class JW_1219_모자이크 {
 		
 		int wrong = Integer.parseInt(br.readLine()); //잘못칠해진칸의 개수
 		
-		List<Point> list = new ArrayList<>();
-		
+		int[] list = new int[wrong];
 		int max_r = 0; //가장 높은 행값 배열의 시작을 위해?(맨 밑에서 시작)
+		int max_c = 0; //
 		for (int i = 0; i < wrong; i++) {
 			st = new StringTokenizer(br.readLine());
 			
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
 			
-			list.add(new Point(x,y));
+			list[i] = y;
 			max_r = Math.max(max_r, x);
+			max_c = Math.max(max_c, y);
 		}
-		
-		Collections.sort(list);
-		
-		int current_c = list.get(0).y+2;
-		for (int i = 0; i < list.size(); i++) {
-			if(current_c < list.get(i).y) {
-				current_c = list.get(i).y;
+		//System.out.println(max_c);
+		boolean flag = false;
+		Arrays.sort(list);
+		int idx = 0;
+		int square;
+		if(list.length==1) {
+			sb.append(1);
+			flag = true;
+		}
+		if(!flag) {
+			while(true) {
+				int cnt = n-1; //색종이의 수 카운트
+				square = max_r + idx; // 사각형 크기 설정
+				if(square>max_c) break;
+				//System.out.println(square+" "+idx);
+				int current_c = list[0] + square; //
+				
+				for (int i = 1; i < list.length; i++) {
+					//현재 칸범위벗어나면 새로운 색종이쓰기.
+					if(current_c <= list[i]) {
+						current_c = list[i]+square;
+						cnt--; //색종이장수 잘라.
+						if(cnt<0) {
+							break;
+						}
+					}
+					//현재 칸 범위 안에 있다면.
+				}
+				if(cnt>=0) {
+					sb.append(square);
+					break;
+				}
+				else {
+					idx++; //사각형크기늘리기.
+				}
 			}
-			
 		}
-		
+		System.out.println(sb);
 	}
 }
 
